504cf0c3435af2fe2d46404b39d3a0da8649460e
Luiz Henrique <luizhjramos@outlook.com>
2022-02-16 22:34:11 -0300

    feat: fetching graphQL query data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,16 @@
 // Packages
 import Head from "next/head";
 import Image from "next/image";
+import { usePageQuery } from "../generated/graphql";
 
 export default function Home() {
+  const [{ data }] = usePageQuery({ 
+    variables: {
+      slug: "about"
+    }
+   }) 
   // -------------------------------------------------
   // Render
   // -------------------------------------------------
-  return <h1>Hello World</h1>;
+  return <h1>{data?.page.title}</h1>;
 }
