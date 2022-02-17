813c23c4dc35de1b5813e4152a432205726774b2
Luiz Henrique <luizhjramos@outlook.com>
2022-02-16 23:00:00 -0300

    feat: Server side function to fething CMS API data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,27 @@
 // Packages
+import { GetServerSideProps } from "next";
 import Head from "next/head";
 import Image from "next/image";
-import { usePageQuery } from "../generated/graphql";
+import { PageDocument, usePageQuery } from "../generated/graphql";
+import { client, ssrCache } from "../lib/urql";
 
 export default function Home() {
-  const [{ data }] = usePageQuery({ 
+  const [{ data }] = usePageQuery({
     variables: {
-      slug: "about"
-    }
-   }) 
+      slug: "about",
+    },
+  });
   // -------------------------------------------------
   // Render
   // -------------------------------------------------
   return <h1>{data?.page.title}</h1>;
 }
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  await client.query(PageDocument, { slug: "about" }).toPromise();
+  return {
+    props: {
+      urqlState: ssrCache.extractData(),
+    },
+  };
+};
